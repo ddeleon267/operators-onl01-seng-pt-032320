75434d808919e5936aba15d182e09133e9d18346
Dominique De León <dominique.a.deleon@gmail.com>
2020-03-26 00:43:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,17 @@
 def unsafe?(speed)
-
+  speed < 40 || speed > 60
+# if speed > 60 || speed < 40
+#   true
+# elsif speed < 60 && speed > 40
+#   false
+# end
+   
 end
 
 
 
 def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
 	
 end
 	
